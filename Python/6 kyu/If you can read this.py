d2dd58608bca30dbf80c41463158a5ce3a7b4343
Telmooo <44447503+Telmooo@users.noreply.github.com>
2018-11-02 21:45:21 +0000

    Create If you can read this.py

---
+++ Python/6 kyu/If you can read this.py
@@ -0,0 +1,7 @@
+def to_nato(words):
+    alphabet = {"a": "Alfa", "b": "Bravo", "c": "Charlie", "d": "Delta", "e": "Echo", "f": "Foxtrot",
+                   "g": "Golf", "h": "Hotel", "i": "India", "j": "Juliett", "k": "Kilo", "l": "Lima",
+                   "m": "Mike", "n": "November", "o": "Oscar", "p": "Papa", "q": "Quebec", "r": "Romeo",
+                   "s": "Sierra", "t": "Tango", "u": "Uniform", "v": "Victor", "w": "Whiskey", "x": "Xray",
+                   "y": "Yankee", "z": "Zulu", ".": ".", "?": "?", "!": "!", ",": ","}
+    return " ".join([alphabet.get(char.lower()) for char in words if char.lower() in alphabet.keys()])
