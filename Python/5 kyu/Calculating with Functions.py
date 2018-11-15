4769057b5a186b2fe4c637f0ca6ce6913276e5d2
Telmo <44447503+Telmooo@users.noreply.github.com>
2018-11-15 12:57:56 +0000

    Create Calculating with Functions.py

---
+++ Python/5 kyu/Calculating with Functions.py
@@ -0,0 +1,15 @@
+def zero(func = ""): return eval(f"0{func}")
+def one(func = ""): return eval(f"1{func}")
+def two(func = ""): return eval(f"2{func}")
+def three(func = ""): return eval(f"3{func}")
+def four(func = ""): return eval(f"4{func}")
+def five(func = ""): return eval(f"5{func}")
+def six(func = ""): return eval(f"6{func}")
+def seven(func = ""): return eval(f"7{func}")
+def eight(func = ""): return eval(f"8{func}")
+def nine(func = ""): return eval(f"9{func}")
+
+def plus(func): return f"+{func}"
+def minus(func): return f"-{func}"
+def times(func): return f"*{func}"
+def divided_by(func): return f"//{func}"
