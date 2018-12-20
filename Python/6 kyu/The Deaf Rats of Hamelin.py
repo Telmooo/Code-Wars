bf3c6ef45391fccf38898bf768e29b9e76f98e71
Telmo <44447503+Telmooo@users.noreply.github.com>
2018-12-20 19:38:14 +0000

    Create The Deaf Rats of Hamelin.py

--- Python/6 kyu/The Deaf Rats of Hamelin.py
+++ Python/6 kyu/The Deaf Rats of Hamelin.py
@@ -1,17 +1,1 @@
-def count_deaf_rats(town):
-    p_loc = town.find("P")
-    skip = False
-    deaf_rats = 0
-    for i, char in enumerate(town):
-        if skip: skip = False; continue
-
-        if char == "~":
-            deaf_rats += 1 if i > p_loc else 0
-            skip = True
-            continue
-
-        if char == "O":
-            deaf_rats += 1 if i < p_loc else 0
-            skip = True
-            continue
-    return deaf_rats
+count_deaf_rats = lambda town: town.replace(' ', '')[::2].count('O')
