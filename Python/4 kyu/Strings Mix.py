7b5e3e9e029c8792537ffcb9039e7c52f591259e
Telmo <44447503+Telmooo@users.noreply.github.com>
2019-01-08 00:44:27 +0000

    Create Strings Mix.py

--- Python/4 kyu/Strings Mix.py
+++ Python/4 kyu/Strings Mix.py
@@ -1,22 +1,9 @@
 def mix(s1, s2):
-    d = {}
-
-    for char in set(s1):
-        if char.islower():
-            occ = s1.count(char)
-            if occ > 1:
-                d[char] = (occ, "1")
-
-    for char in set(s2):
-        if char.islower():
-            occ = s2.count(char)
-            if occ > 1:
-                stored = d.get(char, (0, None))[0]
-                if occ > stored:
-                    d[char] = (occ, "2")
-                elif occ == stored:
-                    d[char] = (occ, "=")
-
-    l = sorted(d.items(), key = lambda x: (-x[1][0], x[1][1], x[0]))
-    
-    return "/".join(f"{i[1][1]}:{i[0]*i[1][0]}" for i in l)
+    compare = lambda x, y: 0 if x > y else 1 if x < y else 2
+    l = []
+    for c in set(s1+s2):
+        if c.islower():
+            occ1, occ2 = s1.count(c), s2.count(c)
+            if occ1 > 1 or occ2 > 1:
+                l.append(f"{'12='[compare(occ1, occ2)]}:{c*max(occ1, occ2)}")
+    return "/".join(sorted(l, key = lambda x: (-len(x), x)))
